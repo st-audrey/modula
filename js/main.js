00b575cb4ae4cd5773680197ce74840df5b35520
Audrey <alphasigmatau@hotmail.com>
2019-11-29 01:19:17 +0100

    Add first draw of static main page

---
+++ js/main.js
@@ -0,0 +1,62 @@
+
+//$(function () {
+//    $("#contact-form").submit(function () {
+//        alert("test");
+//        $.ajax({
+//            url: "" ,
+//            type: 'post',
+//            data: ,
+//            dataType:
+//            success : function(code_html, statut) {
+                
+//            },
+
+//            error: function (resultat, statut, erreur) {
+
+//            },
+
+//            complete: function (resultat, statut) {
+
+//            }
+//        });
+//    });                       
+//});
+// Menu toggle
+
+//var menuBtn = document.getElementById('menu-btn');
+//var menuImg = document.getElementById('menu-img');
+//var menuContact = document.getElementById('menu-link-contact');
+//var menuAdmin = document.getElementById('menu-link-admin');
+
+
+//menuBtn.addEventListener("click", e => {
+//    if (menuImg.src = "img/menu01.png") {
+//        menuImg.src = "img/menu02.png";
+//        //menuContact.style.animate =
+//        menuContact.style.visibility = "visible";
+//        menuAdmin.style.visibility = "visible";
+//    }else{
+//        menuImg.src = "img/menu01.png"; // Marche pas
+//        menuContact.style.visibility = "hidden";
+//        menuAdmin.style.visibility = "hidden";
+//    }
+//});
+
+
+
+//// Ecouteur d'event sur image de la bio :
+
+//var bioImg = document.getElementById("bio-img");
+
+//bioImg.addEventListener("mouseover", e => {
+//    bioImg.src = "img/bio-color.jpg";    
+//});
+
+//bioImg.addEventListener("mouseout", e => {
+//    bioImg.src = "img/bio-neg.jpg";
+//    //bioImg.style.transition = "all 2s";
+//});
+
+
+
+
---
+++ views/header.php
@@ -0,0 +1,10 @@
+<div class="container-fluid main-bg">
+    <div class="row">
+        <div class="col-md-5 mt-2 ml-2">
+            <img id="logo" src="img/logo.png" />
+        </div>
+        <div class="col-md-6 text-right mr-2 mt-2">
+            <button id="menu-btn"><img id="menu-img" src="img/menu01.png" /></button>
+        </div>
+    </div>
+</div>   
